9bdb4379c51990fbb5c5dd44eac8fb0865eacb11
237R1A05T4 <237r1a05t4@cmrtc.ac.in>
2025-10-13 17:03:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, render_template
+import os
+app = Flask(__name__)
+announcements = []
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/announcements', methods=['GET'])
+def get_announcements():
+    return jsonify(announcements)
+@app.route('/announcements', methods=['POST'])
+def post_announcement():
+    data = request.get_json()
+    if 'text' in data and data['text']:
+        announcements.append(data)
+        return jsonify({"message": "Announcement posted"}), 201
+    return jsonify({"error": "Invalid data"}), 400
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
